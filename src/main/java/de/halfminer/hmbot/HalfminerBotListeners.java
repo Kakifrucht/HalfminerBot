2aea814dfe53e2b3bf8c09e134056438f429f0a6
Kakifrucht <Kakifrucht@users.noreply.github.com>
2017-03-25 18:03:24 +0100

    Only listen to events from regular clients

--- src/main/java/de/halfminer/hmbot/HalfminerBotListeners.java
+++ src/main/java/de/halfminer/hmbot/HalfminerBotListeners.java
@@ -1,5 +1,6 @@
 package de.halfminer.hmbot;
 
+import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.TextMessageTargetMode;
 import com.github.theholywaffle.teamspeak3.api.event.ClientJoinEvent;
 import com.github.theholywaffle.teamspeak3.api.event.ClientMovedEvent;
@@ -16,6 +17,7 @@ class HalfminerBotListeners extends TS3EventAdapter {
     private final static Logger logger = LoggerFactory.getLogger(HalfminerBotListeners.class);
 
     private final HalfminerBot bot = HalfminerBot.getInstance();
+    private final TS3Api api = bot.getApi();
     private final BotStorage storage = bot.getStorage();
     private final CommandDispatcher cmd = new CommandDispatcher();
 
@@ -23,7 +25,7 @@ class HalfminerBotListeners extends TS3EventAdapter {
     private final int channelOfBot;
 
     HalfminerBotListeners() {
-        ServerQueryInfo info = bot.getApi().whoAmI();
+        ServerQueryInfo info = api.whoAmI();
         idOfBot = info.getId();
         channelOfBot = info.getChannelId();
     }
@@ -31,19 +33,27 @@ class HalfminerBotListeners extends TS3EventAdapter {
     @Override
     public void onTextMessage(TextMessageEvent e) {
         // Disregard messages the bot creates and make sure it's in the private chat
-        if (e.getInvokerId() != idOfBot && e.getTargetMode().equals(TextMessageTargetMode.CLIENT)) {
+        if (e.getInvokerId() != idOfBot
+                && e.getTargetMode().equals(TextMessageTargetMode.CLIENT)
+                && isRegularClient(e.getInvokerId())) {
             cmd.dispatchCommand(e.getInvokerName(), e.getInvokerId(), e.getMessage());
         }
     }
 
     @Override
     public void onClientJoin(ClientJoinEvent e) {
-        storage.clientJoined(e.getClientDatabaseId(), e.getClientId());
-        clientEnterMessageAndMove(e.getClientId());
+        if (isRegularClient(e.getClientId())) {
+            storage.clientJoined(e.getClientDatabaseId(), e.getClientId());
+            clientEnterMessageAndMove(e.getClientId());
+        }
     }
 
     @Override
     public void onClientMoved(ClientMovedEvent e) {
+
+        if (!isRegularClient(e.getClientId()))
+            return;
+
         // If client joins the bots channel (not when he leaves), also move bot back to his channel if moved out
         if (this.channelOfBot == e.getTargetChannelId()) {
             clientEnterMessageAndMove(e.getClientId());
@@ -53,6 +63,10 @@ class HalfminerBotListeners extends TS3EventAdapter {
                 logger.warn("Couldn't move bot back to his channel");
             }
         }
+    }
+
+    private boolean isRegularClient(int clientId) {
+        return api.getClientInfo(clientId).isRegularClient();
     }
 
     private void clientEnterMessageAndMove(int clientId) {
