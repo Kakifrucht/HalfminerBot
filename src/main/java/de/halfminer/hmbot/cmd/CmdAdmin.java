4a6d0406671d45e422b7e747dc6b5e5631d8d91b
Kakifrucht <prieto@wunderlich.pw>
2022-02-08 06:28:14 +0100

    Improved !admin lookup command Parsed client created and last connected timestamps Fixed error message when no client found Added !admin info and !admin whois aliases Cleanup

--- src/main/java/de/halfminer/hmbot/cmd/CmdAdmin.java
+++ src/main/java/de/halfminer/hmbot/cmd/CmdAdmin.java
@@ -8,6 +8,9 @@ import de.halfminer.hmbot.storage.HalfClient;
 import de.halfminer.hmbot.util.MessageBuilder;
 import de.halfminer.hmbot.util.StringArgumentSeparator;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -37,129 +40,130 @@ class CmdAdmin extends Command {
             case "stop":
             case "restart":
                 boolean restart = argument.startsWith("r");
-                if (restart) {
-                    sendMessage("cmdAdminRestarted");
-                } else {
-                    sendMessage("cmdAdminStopped");
-                }
+                sendMessage(restart ? "cmdAdminRestarted" : "cmdAdminStopped");
                 componentHolder.getStateHolder().stop("Bot was " + (restart ? "restarted" : "stopped")
                         + " via command by client " + clientInfo.getNickname(), restart);
                 return;
             case "reload":
-                if (componentHolder.getStateHolder().reloadConfig()) {
-                    sendMessage("cmdAdminConfigReloaded");
-                } else {
-                    sendMessage("cmdAdminConfigReloadedError");
-                }
+                boolean success = componentHolder.getStateHolder().reloadConfig();
+                sendMessage("cmdAdminConfigReloaded" + (success ? "" : "Error"));
                 return;
             case "lookup":
-
+            case "info":
+            case "whois":
                 if (command.meetsLength(2)) {
-
-                    String lookupArg = command.getArgument(1);
-
-                    // determine what input we have
-                    LookupType type;
-                    if (lookupArg.length() == 28 && lookupArg.endsWith("=")) {
-                        type = LookupType.STRING_UID;
-                    } else if (command.getArgumentInt(1) > Integer.MIN_VALUE) {
-                        type = LookupType.NUMERIC_ID;
-                    } else {
-                        type = LookupType.NICKNAME;
-                    }
-
-                    Map<String, String> mapToSend = null;
-                    boolean isOnline = false;
-
-                    switch (type) {
-                        case NICKNAME:
-
-                            try {
-                                List<Client> clients = api.getClientsByName(lookupArg);
-                                if (clients.size() == 1) {
-                                    mapToSend = getClientInfoMap(clients.get(0));
-                                    isOnline = true;
-                                } else {
-                                    StringBuilder send = new StringBuilder();
-                                    for (Client client : clients) {
-                                        send.append(" ID: ")
-                                                .append(client.getId())
-                                                .append(" - Nickname: ")
-                                                .append(client.getNickname())
-                                                .append("\n");
-                                    }
-                                    sendMessage("cmdAdminLookupList", "LIST", send.toString());
-                                    return;
-                                }
-
-                            } catch (TS3CommandFailedException ignored) {}
-                            break;
-                        case NUMERIC_ID:
-
-                            int id = command.getArgumentInt(1);
-                            for (Client client : api.getClients()) {
-                                if (client.getId() == id || client.getDatabaseId() == id) {
-                                    mapToSend = getClientInfoMap(client);
-                                    isOnline = true;
-                                    break;
-                                }
-                            }
-
-                            // offline database id lookup
-                            if (mapToSend == null) {
-                                try {
-                                    DatabaseClient dbClient = api.getDatabaseClientInfo(id);
-                                    mapToSend = dbClient.getMap();
-                                } catch (TS3CommandFailedException ignored) {}
-                            }
-
-                            break;
-                        case STRING_UID:
-
-                            try {
-                                Client client = api.getClientByUId(lookupArg);
-                                mapToSend = getClientInfoMap(client);
-                                isOnline = true;
-                            } catch (TS3CommandFailedException e) {
-
-                                try {
-                                    DatabaseClient dbClient = api.getDatabaseClientByUId(lookupArg);
-                                    mapToSend = dbClient.getMap();
-                                } catch (TS3CommandFailedException ignored) {}
-                            }
-                            break;
-                    }
-
-                    if (mapToSend == null) {
-                        sendMessage("cmdAdminLookupNotFound");
-                        return;
-                    }
-
-                    StringBuilder toSend = new StringBuilder("======= ")
-                            .append(mapToSend.get("client_nickname"))
-                            .append(" (")
-                            .append(isOnline ? "Online" : "Offline")
-                            .append(", DBID: ")
-                            .append(mapToSend.get("client_database_id"))
-                            .append(") =======\n");
-
-                    for (Map.Entry<String, String> entry : mapToSend.entrySet()) {
-                        if (entry.getValue().length() == 0) continue;
-                        toSend.append(" ")
-                                .append(entry.getKey())
-                                .append("=")
-                                .append(entry.getValue())
-                                .append("\n");
-                    }
-
-                    MessageBuilder.create(toSend.toString())
-                            .setDirectString()
-                            .sendMessage(clientId);
+                    doLookup();
                     return;
                 }
             default:
                 throw new InvalidCommandException(CommandEnum.ADMIN);
         }
+    }
+
+    private void doLookup() {
+
+        String lookupArg = command.getArgument(1);
+
+        // determine what input we have
+        LookupType type;
+        if (lookupArg.length() == 28 && lookupArg.endsWith("=")) {
+            type = LookupType.STRING_UID;
+        } else if (command.getArgumentInt(1) > Integer.MIN_VALUE) {
+            type = LookupType.NUMERIC_ID;
+        } else {
+            type = LookupType.NICKNAME;
+        }
+
+        Map<String, String> mapToSend = null;
+        boolean isOnline = false;
+
+        switch (type) {
+            case NICKNAME:
+
+                try {
+                    List<Client> clients = api.getClientsByName(lookupArg);
+                    if (clients.size() == 1) {
+                        mapToSend = getClientInfoMap(clients.get(0));
+                        isOnline = true;
+                    } else if (!clients.isEmpty()) {
+                        StringBuilder send = new StringBuilder();
+                        for (Client client : clients) {
+                            send.append(" ID: ")
+                                    .append(client.getId())
+                                    .append(" - Nickname: ")
+                                    .append(client.getNickname())
+                                    .append("\n");
+                        }
+                        sendMessage("cmdAdminLookupList", "LIST", send.toString());
+                        return;
+                    }
+
+                } catch (TS3CommandFailedException ignored) {}
+                break;
+            case NUMERIC_ID:
+
+                int id = command.getArgumentInt(1);
+                for (Client client : api.getClients()) {
+                    if (client.getId() == id || client.getDatabaseId() == id) {
+                        mapToSend = getClientInfoMap(client);
+                        isOnline = true;
+                        break;
+                    }
+                }
+
+                // offline database id lookup
+                if (mapToSend == null) {
+                    try {
+                        DatabaseClient dbClient = api.getDatabaseClientInfo(id);
+                        mapToSend = dbClient.getMap();
+                    } catch (TS3CommandFailedException ignored) {}
+                }
+
+                break;
+            case STRING_UID:
+
+                try {
+                    Client client = api.getClientByUId(lookupArg);
+                    mapToSend = getClientInfoMap(client);
+                    isOnline = true;
+                } catch (TS3CommandFailedException e) {
+
+                    try {
+                        DatabaseClient dbClient = api.getDatabaseClientByUId(lookupArg);
+                        mapToSend = dbClient.getMap();
+                    } catch (TS3CommandFailedException ignored) {}
+                }
+                break;
+        }
+
+        if (mapToSend == null) {
+            sendMessage("cmdAdminLookupNotFound");
+            return;
+        }
+
+        mapToSend.put("client_created", stringToDate(mapToSend.get("client_created")));
+        mapToSend.put("client_lastconnected", stringToDate(mapToSend.get("client_lastconnected")));
+
+        StringBuilder toSend = new StringBuilder("======= ")
+                .append(mapToSend.get("client_nickname"))
+                .append(" (")
+                .append(isOnline ? "Online" : "Offline")
+                .append(", DBID: ")
+                .append(mapToSend.get("client_database_id"))
+                .append(") =======\n");
+
+        for (Map.Entry<String, String> entry : mapToSend.entrySet()) {
+            if (entry.getValue().length() == 0) continue;
+            toSend.append(" ")
+                    .append(entry.getKey())
+                    .append("=")
+                    .append(entry.getValue())
+                    .append("\n");
+        }
+
+        MessageBuilder.create(toSend.toString())
+                .setDirectString()
+                .sendMessage(clientId);
     }
 
     private Map<String, String> getClientInfoMap(Client client) {
@@ -171,6 +175,21 @@ class CmdAdmin extends Command {
             } catch (TS3CommandFailedException e) {
                 return null;
             }
+        }
+    }
+
+    private String stringToDate(String timestampStr) {
+        try {
+            long timestamp = Long.parseLong(timestampStr);
+            Instant instant = Instant.ofEpochSecond(timestamp);
+
+            return DateTimeFormatter
+                    .ofPattern("yyyy-MM-dd HH:mm")
+                    .withZone(ZoneId.systemDefault())
+                    .format(instant);
+        } catch (NumberFormatException e) {
+            logger.warn("Couldn't parse timestamp {}", timestampStr);
+            return timestampStr;
         }
     }
 
